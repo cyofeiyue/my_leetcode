6c44aadff567ae95ce7349f9ae8d54d84fdd7c09
zhangfy <feiyue_zh@163.com>
2020-05-13 17:25:06 +0800

    144_二叉树前序遍历

--- src/com/feiyue/cn/二叉树/_144_二叉树的前序遍历.java
+++ src/com/feiyue/cn/二叉树/_144_二叉树的前序遍历.java
@@ -1,5 +1,42 @@
 package com.feiyue.cn.二叉树;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import com.feiyue.cn.common.TreeNode;
+
+/**
+   *  使用栈，时间复杂度O(n)，空间复杂度O(n)
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/submissions/
+ * @author feiyue
+ *
+ */
 public class _144_二叉树的前序遍历 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+    	List<Integer> result = new ArrayList<>();
+    	if (root == null) return result;
+    	
+    	Stack<TreeNode> stack = new Stack<>();
+    	
+    	stack.push(root);
+    	while (!stack.isEmpty()) {
+    		TreeNode node = stack.pop();
+			result.add(node.val);
+			
+			if (node.right != null) {
+				stack.push(node.right);
+			}
+			
+			if (node.left != null) {
+				stack.push(node.left);
+			}
+
+		}
+    	
+    	return result;
+    	
+    }
+    
 
 }
