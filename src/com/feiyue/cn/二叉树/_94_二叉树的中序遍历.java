90ede76f6d1f280c2cbf8a4ce968f8028c304129
zhangfy <feiyue_zh@163.com>
2020-05-14 12:47:12 +0800

    _94_二叉树的中序遍历 题解

--- src/com/feiyue/cn/二叉树/_94_二叉树的中序遍历.java
+++ src/com/feiyue/cn/二叉树/_94_二叉树的中序遍历.java
@@ -1,5 +1,32 @@
 package com.feiyue.cn.二叉树;
 
+import com.feiyue.cn.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class _94_二叉树的中序遍历 {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        
+        TreeNode p = root;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (!stack.isEmpty() || p != null) {
+            if (p != null) {
+                stack.push(p);
+                p = p.left;
+            } else {
+                p = stack.pop();
+                list.add(p.val);
+                p = p.right;
+            }
+        }
+
+        return list;
+    }
+
 
 }
