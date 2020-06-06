bc9df819779e1c25ad1a73ad25952e675e119925
feiyue zhang <feiyue_zh@163.com>
2020-06-06 13:27:21 +0800

    75.颜色分类

--- src/com/feiyue/cn/数组/_75_颜色分类.java
+++ src/com/feiyue/cn/数组/_75_颜色分类.java
@@ -1,7 +1,13 @@
 package com.feiyue.cn.数组;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 /**
- * 套路：扫描一遍数组，一般借助双指针甚至双指针
+ * 套路：题意要求扫描一遍数组，一般借助双指针甚至双指针
  * 
  * 红色指针用来扫描数据。
  * 遇到1：跳过，红色指针++
@@ -12,27 +18,63 @@ package com.feiyue.cn.数组;
  *
  */
 public class _75_颜色分类 {
+//    public void sortColors(int[] nums) {
+//    	int i = 0; //红色指针
+//    	int l = 0; //绿色指针
+//    	int r = nums.length - 1; //紫色指针
+//    	//退出循环的条件是：红色指针i > 紫色指针r
+//    	while (i <= r) {
+//			int value = nums[i];
+//			if (value == 0) {
+//				swapValues(nums, i++, l++);
+//			} else if (value == 1) {
+//				i++;
+//			} else {
+//				swapValues(nums, i, l--);
+//			}
+//		}
+//    	
+//    }
+//    
+//    private void swapValues(int[] nums, int i, int j) {
+//    	int tmp = nums[i];
+//    	nums[i] = nums[j];
+//    	nums[j] = tmp;
+//    }
     public void sortColors(int[] nums) {
-    	int i = 0; //红色指针
-    	int l = 0; //绿色指针
-    	int r = nums.length - 1; //紫色指针
-    	//退出循环的条件是：红色指针i > 紫色指针r
+    	int i = 0; //红色 用来扫描诉诸
+    	int l = 0; //绿色
+    	int r = nums.length - 1; //紫色
+    	
     	while (i <= r) {
-			int value = nums[i];
-			if (value == 0) {
-				swapValues(nums, i++, l++);
-			} else if (value == 1) {
+    		int v = nums[i];
+    		if (v == 0) {
+				swap(nums, i++, l++);
+			} else if (v == 1) {
 				i++;
 			} else {
-				swapValues(nums, i, l--);
+				swap(nums, i, r--);
 			}
+			
 		}
-    	
+
     }
     
-    private void swapValues(int[] nums, int i, int j) {
-    	int tmp = nums[i];
-    	nums[i] = nums[j];
-    	nums[j] = tmp;
-    }
+   
+    private void swap(int[] nums, int i, int j) {
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+	}
+    
+    public static void main(String[] args) {
+		int[] nums = new int[] {2,0,2,1,1,0};
+		_75_颜色分类  xxx = new _75_颜色分类();
+		xxx.sortColors(nums);
+		
+		System.out.println(Arrays.stream( nums ).boxed().collect(Collectors.toList()));
+		
+		
+	}
+    
 }
